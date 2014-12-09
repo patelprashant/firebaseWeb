f97f30d24d39eeff7234cc77570c79c684e5a0f9
Prashant <prashant.patel77@gmail.com>
2014-12-10 10:56:16 +1100

    Added capitalization of first letter

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -14,12 +14,12 @@ angular.module('firebaseWebApp')
     //   'AngularJS',
     //   'Karma'
     // ];
-     var ref = new Firebase("https://blistering-heat-3711.firebaseio.com/Contacts");
-     var Person = $firebase(ref).$asArray();
+    var ref = new Firebase("https://blistering-heat-3711.firebaseio.com/Contacts");
+    var Person = $firebase(ref).$asArray();
 
     $scope.person = Person;
 
-    $scope.add = function(){
+    $scope.add = function () {
       var save = Person.$add({
         firstName: $scope.firstName,
         lastName: $scope.lastName
@@ -28,29 +28,49 @@ angular.module('firebaseWebApp')
       $scope.firstName = '';
       $scope.lastName = '';
 
-      if(save){
+      if (save) {
         console.log('Pass: Saved Successfully');
         LxNotificationService.success('Added');
-      }
-      else{
+      } else {
         console.log('Faild: Something wrong went');
         LxNotificationService.error('Something went wrong');
       }
     };
-  
-    $scope.remove = function(id){
+
+    $scope.remove = function (id) {
       Person.$remove(id);
       LxNotificationService.warning('Removed');
     }
   })
-  .controller('EditCtrl', function($scope, $location, $routeParams, $firebase, LxNotificationService){
+  .controller('EditCtrl', function ($scope, $location, $routeParams, $firebase, LxNotificationService) {
     var fbURL = "https://blistering-heat-3711.firebaseio.com/Contacts/";
     var personURL = new Firebase(fbURL + $routeParams.id);
     $scope.person = $firebase(personURL).$asObject();
 
-    $scope.edit = function(){
+    $scope.edit = function () {
       $scope.person.$save();
       LxNotificationService.success('Saved');
       $location.path('/');
     }
+  })
+  .directive('capitalizeFirst', function ($parse) {
+    return {
+      require: 'ngModel',
+      link: function (scope, element, attrs, modelCtrl) {
+        var capitalize = function (inputValue) {
+          if (inputValue === undefined) {
+            inputValue = '';
+          }
+          var capitalized = inputValue.charAt(0).toUpperCase() +
+            inputValue.substring(1);
+          if (capitalized !== inputValue) {
+            modelCtrl.$setViewValue(capitalized);
+            modelCtrl.$render();
+          }
+          return capitalized;
+        }
+        modelCtrl.$parsers.push(capitalize);
+        capitalize($parse(attrs.ngModel)(scope)); // capitalize initial value
+      }
+    };
   });
