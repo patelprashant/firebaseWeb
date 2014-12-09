27cc4cb23e985e6f72922f847a89fd4849fad075
Prashant <prashant.patel77@gmail.com>
2014-12-09 11:14:16 +1100

    Fixed font icon on hosting

--- Gruntfile.js
+++ Gruntfile.js
@@ -320,6 +320,11 @@ module.exports = function (grunt) {
           cwd: 'bower_components/bootstrap/dist',
           src: 'fonts/*',
           dest: '<%= yeoman.dist %>'
+        }, {
+          expand: true,
+          cwd: 'bower_components/lumx/dist',
+          src: 'fonts/md-icons/*',
+          dest: '<%= yeoman.dist %>'
         }]
       },
       styles: {
